6ac5d154ef450c751d919860e9b108265837f94e
Dennis Gebhardt <dgebhard@stud.hs-heilbronn.de>
2018-04-10 10:19:48 +0200

    laborGames

---
+++ Half Life 4/Assets/BombScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour {
+    public float lifeTime;
+    public float explosionForce;
+    public float explosionRadius;
+    public float upModifier;
+    public LayerMask bombMask;
+    public GameObject explosionEffect;
+	// Use this for initialization
+	void Start () {
+        Invoke("explode", lifeTime);
+	}
+	
+	// Update is called once per frame
+	void explode () {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius, bombMask);
+        foreach(Collider c in hitColliders)
+        {
+            Rigidbody r = c.GetComponent<Rigidbody>();
+            if (r != null)
+                r.AddExplosionForce(explosionForce, transform.position, explosionRadius, upModifier);
+        }
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        gameObject.GetComponent<AudioSource>().Play();
+        gameObject.GetComponent<Renderer>().enabled = false;
+        Invoke("destroy", 2);
+	}
+    void destroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Half Life 4/Assets/PlayerControls.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+    public Camera cam;
+    public LayerMask interactionLayer;
+    public float maxRange;
+    public Transform handPosition;
+    public float throwForce;
+    public float slowMoFactor;
+    public GameObject bombPrefab;
+    private Rigidbody objInHand;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if(Time.timeScale == 1)
+            {
+                Time.timeScale = slowMoFactor;
+              
+            }
+            else
+                Time.timeScale = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (objInHand == null)
+            {
+
+
+                RaycastHit hit;
+                Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+                Debug.DrawLine(ray.origin, ray.GetPoint(maxRange));
+                if (Physics.Raycast(ray, out hit, maxRange, interactionLayer))
+                {
+                    objInHand = hit.transform.GetComponent<Rigidbody>();
+                    objInHand.isKinematic = true;
+
+                    objInHand.transform.position = handPosition.position;
+                    objInHand.transform.parent = handPosition;
+                }
+            }
+
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (objInHand != null)
+            {
+                objInHand.isKinematic = false;
+                objInHand.transform.parent = null;
+                objInHand.AddForce(cam.transform.forward * throwForce);
+                objInHand = null;
+            }
+            else
+            {
+                GameObject bomb = Instantiate(bombPrefab, handPosition.position, Quaternion.identity);
+                bomb.GetComponent<Rigidbody>().AddForce(cam.transform.forward * throwForce);
+            }
+
+        }
+    }
+}
